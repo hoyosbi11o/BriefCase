b3ee101c2f4a1a63bc8d90161dceb76138c0149a
hoyosbi11o <hoyosboli20@gmail.com>
2024-02-26 15:43:39 -0500

    second update code

--- src/components/SectionIcons/SectionIcons.jsx
+++ src/components/SectionIcons/SectionIcons.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { CiLinkedin, CiFacebook, CiInstagram, CiTwitter } from "react-icons/ci";
 import { FaWhatsapp } from "react-icons/fa";
-
+import "./SectionIcons.css"
 export const SectionIcons = () => {
   return (
-    <div>
-          <CiLinkedin />
-          <CiFacebook />
-          <CiInstagram />
-          <CiTwitter />
-          <FaWhatsapp />
+    <div className='SectionIco'>
+        <CiLinkedin className='Icon' />
+        <CiFacebook className='Icon' />
+        <CiInstagram className='Icon' />
+        <CiTwitter className='Icon' />
+        <FaWhatsapp className='Icon' />
 
     </div>
    
--- src/components/SliderBar/SilderBar.jsx
+++ src/components/SliderBar/SilderBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./SilderBar"
+import "./SliderBar.css"
 export const SilderBar = ({children}) => {
   return (
     <nav>
