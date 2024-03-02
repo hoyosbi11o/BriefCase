7db53b07d3bdd6c54a7b02035aba7bf4efbe7c58
hoyosbi11o <hoyosboli20@gmail.com>
2024-03-02 14:45:24 -0500

    update code

--- src/components/ContentJson/ContentCard.js
+++ src/components/ContentJson/ContentCard.js
@@ -2,7 +2,7 @@ export const ContentCard =[
     {
         _id: 1,
         title: "Mongo Work",
-        image: "../../public/MongoDB.jpg",
+        image: "../../../public/MongoDB.jpg",
         description: "Description",
         enlace:"Ir a proyecto"
     
@@ -10,7 +10,7 @@ export const ContentCard =[
     {
         _id: 2,
         title: "Api Rest",
-        image: "../../public/apiRest.png",
+        image: "../../../public/apiRest.png",
         description: "Description",
         enlace:"Ir a proyecto"
     
@@ -18,7 +18,7 @@ export const ContentCard =[
     {
         _id: 3,
         title: "Bleux",
-        image: "../../public/bleux.jpg",
+        image: "../../../public/bleux.jpg",
         description: "Description",
         enlace:"Ir a proyecto"
     
@@ -26,7 +26,7 @@ export const ContentCard =[
     {
         _id: 4,
         title: "Proyecto react",
-        image: "../../public/bleux.jpg",
+        image: "../../../public/bleux.jpg",
         description: "Description",
         enlace:"Ir a proyecto"
     },
